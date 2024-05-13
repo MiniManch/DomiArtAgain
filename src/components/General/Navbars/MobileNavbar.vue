2f45de074bc60283787149e58d4dd6264f6b9834
mano <emanuelrokah96@gmail.com>
2024-05-13 13:07:15 +0300

    finishing navbar mobile animation and everything to do with navbar!

--- src/components/General/Navbars/MobileNavbar.vue
+++ src/components/General/Navbars/MobileNavbar.vue
@@ -1,48 +1,94 @@
 <template>
-      <nav class="navbar-fixed">
-        <div class="hamburger-icon" @click="toggleNavbar" v-if="!isOpen" @scroll="Handlescroll">
-          <img src="../../../../public/images/icons/menu/menu.png" alt="Menu">
-        </div>
-        <ul :class="{ 'navbar-open': isOpen }" v-if="isOpen">
-          <li @click="toggleNavbar">X</li>
-          <li><router-link :to="{ name: 'home' }">Home</router-link></li>
-          <li><router-link :to="{ name: 'about' }">About</router-link></li>
-          <li><router-link :to="{ name: 'gallery' }">Gallery</router-link></li>
-          <li><router-link :to="{ name: 'contact' }">Contact</router-link></li>
-        </ul>
-      </nav>
-  </template>
-  
-  <script>
-  import 'animate.css';
-  
-  export default {
-    name: 'MobileNavbar',
-    data() {
-      return {
-        isOpen: false
+  <nav class="navbar-fixed">
+    <div :class="['hamburger-icon','animate__animated',hamburgerAnimation]" @click=" animating ? null : toggleNavbar('open')" v-if="!animating && !isOpen" @scroll="Handlescroll">
+      <img src="../../../../public/images/icons/menu/menu.png" alt="Menu">
+    </div>
+    <ul :class="[isOpen ? 'navbar-open' : null, animating ? animation : null, 'animate__animated' ]" v-if="isOpen || animating" class="animated-navbar">
+      <li @click="toggleNavbar('close')" class="closeButton">X</li>
+      <li><router-link :to="{ name: 'home' }">Home</router-link></li>
+      <li><router-link :to="{ name: 'about' }">About</router-link></li>
+      <li><router-link :to="{ name: 'gallery' }">Gallery</router-link></li>
+      <li><router-link :to="{ name: 'contact' }">Contact</router-link></li>
+    </ul>
+  </nav>
+</template>
+
+<script>
+import 'animate.css';
+
+export default {
+  name: 'MobileNavbar',
+  data() {
+    return {
+      isOpen: false,
+      animating: false,
+      animation: null,
+      hamburgerAnimation: null,
+    }
+  },
+  methods: {
+    toggleNavbar(direction) {
+      this.toggleAnimation(direction);
+    },
+    toggleAnimation(direction) {
+      this.hamburgerAnimation = null;
+      if (!this.animating) {
+        this.animating = true;
+        if (direction === 'open') {
+          this.animation = 'animate__fadeInDown';
+        } else if (direction === 'close') {
+          this.animation = 'animate__fadeOutUp';
+        }
+        setTimeout(() => {
+          this.isOpen = !this.isOpen;
+          this.animating = false;
+          this.animation = null;
+        }, 800);
+        this.hamburgerAnimation = 'animate__fadeIn';
       }
     },
-    methods: {
-      toggleNavbar() {
-        this.isOpen = !this.isOpen;
-      },
-    }
   }
-  </script>
-  
-  <style scoped>
-  @import url('https://fonts.googleapis.com/css2?family=Comfortaa:wght@300;400&display=swap');
-  
-  .navbar-fixed {
-    position: fixed;
-    top: 1vh;
-    left: 1vh;
-    z-index: 1000;
-  }
+}
+</script>
 
-  .hamburger-icon > img {
-    width:40px;
-  }
-  </style>
-  
+<style scoped>
+@import url('https://fonts.googleapis.com/css2?family=Comfortaa:wght@300;400&display=swap');
+
+.navbar-fixed {
+  position: fixed;
+  z-index: 1000;
+}
+.hamburger-icon {
+  position: relative;
+  left: 1vh;
+  top: 1vh;
+}
+.hamburger-icon > img {
+  width: 40px;
+}
+ul {
+  padding-left: 1vh;
+  padding-right: 1vh;
+  background-color: #5E5343;
+  display: flex;
+  flex-direction: column;
+}
+
+li {
+  list-style: none;
+  display: flex;
+  justify-content: flex-start;
+}
+li:last-child {
+  padding-bottom: 1vh;
+}
+a {
+  text-decoration: none;
+  font-family: "Playfair Display", serif;
+  color: #EFE9E4;
+}
+.closeButton {
+  width: fit-content;
+  color: #EFE9E4;
+}
+</style>
