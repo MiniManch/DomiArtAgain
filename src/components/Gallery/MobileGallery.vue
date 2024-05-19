4d9f7c796f72e1e38f348bc7f4a5b37e844a1446
mano <emanuelrokah96@gmail.com>
2024-05-19 17:47:11 +0300

    adding scrollOnto up to the title of the gallery on each change of page

--- src/components/Gallery/MobileGallery.vue
+++ src/components/Gallery/MobileGallery.vue
@@ -1,28 +1,28 @@
 <template>
-    <div class='everything' v-if=" paginatedItems">
-        <h1>Gallery</h1>
-        <nav class="pagniationItem" >
-            <ul class="pagination pagination-sm mb-0 pb-5" >
-                <li class="page-item" v-for="(item, index) in paginatedItems" :key="index" >
-                    <span :class="['page-link',(index+1) === activePage ? 'activePage' : null]" @click="changeActivePage(index+1)">{{index + 1}}</span>
+    <div class='everything' v-if="paginatedItems">
+        <h1 ref="galleryTitle">Gallery</h1>
+        <nav class="pagniationItem">
+            <ul class="pagination pagination-sm mb-0 pb-5">
+                <li class="page-item" v-for="(item, index) in paginatedItems" :key="index">
+                    <span :class="['page-link', (index+1) === activePage ? 'activePage' : null]" @click="changeActivePage(index+1)">{{index + 1}}</span>
                 </li>
             </ul>
         </nav>
 
         <div class="galleryContainer">
             <div class="row row-cols-1 row-cols-md-3 g-4 w-100">
-                <div class="col mb-5 " v-for="(item, index) in paginatedItems[this.activePage -1]" :key="index">
-                    <img :src="item.link_1" class="card-img-top image" alt="..." @click="openImage(item.id)" >
+                <div class="col mb-5" v-for="(item, index) in paginatedItems[this.activePage - 1]" :key="index">
+                    <img :src="item.link_1" class="card-img-top image" alt="..." @click="openImage(item.id)">
                     <h5 class="image-title">{{item.title}}</h5>
                 </div>
             </div>
         </div>
 
-        <!-- Pagniation -->
-        <nav class="pagniationItem" >
-            <ul class="pagination pagination-sm mb-0 pb-5" >
-                <li class="page-item" v-for="(item, index) in paginatedItems" :key="index" >
-                    <span :class="['page-link',(index+1) === activePage ? 'activePage' : null]" @click="changeActivePage(index+1)">{{index + 1}}</span>
+        <!-- Pagination -->
+        <nav class="pagniationItem">
+            <ul class="pagination pagination-sm mb-0 pb-5">
+                <li class="page-item" v-for="(item, index) in paginatedItems" :key="index">
+                    <span :class="['page-link', (index+1) === activePage ? 'activePage' : null]" @click="changeActivePage(index+1)">{{index + 1}}</span>
                 </li>
             </ul>
         </nav>
@@ -30,7 +30,6 @@
     <!-- Loading Modal -->
     <LoadingModal v-if="isLoading"/>
 </template>
-  
 
 <script>
 import imageData from "../../../public/data/GalleryImages.json";
@@ -38,12 +37,12 @@ import LoadingModal from "../General/LoadingModal.vue";
 
 export default {
     data(){
-        return{
+        return {
             data: null,
             paginatedItems: null,
-            activePage:1,
-            maxPages:null,
-            isLoading:false
+            activePage: 1,
+            maxPages: null,
+            isLoading: false
         }
     },
     mounted(){
@@ -62,65 +61,68 @@ export default {
             if(this.activePage != pageNum){
                 this.activePage = pageNum;
                 this.isLoading = true;
-                setTimeout(() =>{this.isLoading = false;},1200)
-                window.scrollTo({ top: 0 });
+                this.scrollToTitle();
+                setTimeout(() => {
+                    this.isLoading = false;
+                }, 1200);
             }
+        },
+        scrollToTitle() {
+            this.$refs.galleryTitle.scrollIntoView({ behavior: 'smooth' });
         },
         openImage(paintingId) {
             this.$router.push({ name: 'painting-detail', params: { id: paintingId } });
         },
     },
-    components:{
+    components: {
         LoadingModal
     }
 }
 </script>
 
 <style scoped>
-    .everything{
+    .everything {
         background-color: #EFE9E4;
         display: flex;
         flex-direction: column;
         align-items: stretch;
-        width:100%;
+        width: 100%;
     }
-    h1{
-        color:#5E5343;
+    h1 {
+        color: #5E5343;
         font-family: "Playfair Display", serif;
         font-size: 6vh;
         margin-bottom: 5vh;
     }
-    .image{
-        max-height:350px;
+    .image {
+        max-height: 350px;
         margin-bottom: 2%;
     }
-    .image-title{
+    .image-title {
         background-color: #5E5343;
-        margin-bottom:0;
+        margin-bottom: 0;
         font-family: 'Comfortaa', sans-serif;
-        color:#EFE9E4;
+        color: #EFE9E4;
     }
-    .pagniationItem{
+    .pagniationItem {
         align-self: center;
     }
-    .page-link{
+    .page-link {
         font-family: 'Comfortaa', sans-serif;
-        font-size:0.8em;
+        font-size: 0.8em;
         background-color: #5E5343;
         border: 1px solid #5E5343;
-        color:#EFE9E4
+        color: #EFE9E4;
     }
-    .activePage{
+    .activePage {
         background-color: #EFE9E4;
         border: 1px solid #EFE9E4;
-        color:#5E5343;
+        color: #5E5343;
     }
-
-    .row,.col{
+    .row, .col {
         padding: 0;
     }
-
-    .galleryContainer{
+    .galleryContainer {
         display: flex;
         justify-content: center;
     }
