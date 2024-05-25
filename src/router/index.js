fb259954d6e2906da8cdc5de991c621af44f20fe
MiniManch <Emaneulrokah96@gmail.com>
2024-05-25 23:48:56 +0300

    Had error with router on build run from netlify, changes made to router file also added netlify.toml file

--- src/router/index.js
+++ src/router/index.js
@@ -1,28 +1,26 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import Home from '../components/Home/Home.vue';
-import GalleryItem from '../components/Gallery/GalleryItemLandscape.vue'; // Assuming you have a component for painting details
-import GalleryItemMobile from '../components/Gallery/GalleryItemMobile.vue';
 
 const routes = [
-    { path: '/', component: Home,name: 'home'},
-    { path: '/about', component: Home,name: 'about'},
-    { path: '/contact', component: Home,name: 'contact'},
-    { path: '/gallery', component: Home,name: 'gallery'},
-    { path: '/painting/:id', component: determineGalleryItemComponent, name: 'painting-detail' } 
+    { path: '/', component: Home, name: 'home' },
+    { path: '/about', component: Home, name: 'about' },
+    { path: '/contact', component: Home, name: 'contact' },
+    { path: '/gallery', component: Home, name: 'gallery' },
+    { path: '/painting/:id', component: () => determineGalleryItemComponent(), name: 'painting-detail' }
 ];
 
 function determineGalleryItemComponent() {
-    const isMobile = window.innerWidth < 768; // Example breakpoint for mobile view
-    
-    // Return appropriate component
-    return isMobile ? GalleryItemMobile : GalleryItem;
+    const isMobile = window.innerWidth < 768;
+
+    // Dynamic import
+    return isMobile
+        ? import('../components/Gallery/GalleryItemMobile.vue')
+        : import('../components/Gallery/GalleryItemLandscape.vue');
 }
 
 const router = createRouter({
     history: createWebHistory(),
     routes
 });
-
-console.log(Home)
 
 export default router;
