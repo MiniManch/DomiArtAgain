84bb15a74f13ef30821d7ffe42a31e8ae68d3c45
mano <emanuelrokah96@gmail.com>
2024-07-24 15:21:35 +0300

    returning shit

--- src/components/Gallery/LandscapeGalleryComponent.vue
+++ src/components/Gallery/LandscapeGalleryComponent.vue
@@ -64,7 +64,7 @@ export default {
       structure: ImageStructure,
       animating: false,
       animation: null,
-      animation_duration: 500,
+      animation_duration: 1000,
       maxPage: 5,
       minPage: 1,
       showModal: false,
